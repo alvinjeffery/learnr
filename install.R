59ba46275c72144a79db42805451c09a87ec0139
Alvin D. Jeffery <alvinjeffery@gmail.com>
2018-04-27 20:11:22 -0500

    added binder button and R packages install

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages(c('shiny', 'd3heatmap', 'scales', 'rms', 'data.table', 'dplyr', 'lubridate', 'stringr', 'tidyr',
+                   'gplots', 'DT', 'cluster', 'fpc', 'mlbench', 'dendextend', 'mclust'),
+                 dependencies = T)
