d6c1f559f13140569926d55390a9c1844e605d4e
Alvin D. Jeffery <alvinjeffery@gmail.com>
2018-04-28 13:44:36 -0500

    limited installation packages

--- install.R
+++ install.R
@@ -1,3 +1,2 @@
-install.packages(c('shiny', 'd3heatmap', 'scales', 'rms', 'data.table', 'dplyr', 'lubridate', 'stringr', 'tidyr',
-                   'gplots', 'DT', 'cluster', 'fpc', 'mlbench', 'dendextend', 'mclust'),
+install.packages(c('rms', 'data.table', 'dplyr', 'lubridate', 'stringr', 'tidyr', 'DT', 'knitr'),
                  dependencies = T)
